329cd934a711f79e514ceee3398ee2fb0ca51169
StanislavVNikitin <stasvnikitin@mail.ru>
2021-03-18 21:45:45 +0300

    update commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInit3d4b66e2d73740a3891bfdeae680c420
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit3d4b66e2d73740a3891bfdeae680c420', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit3d4b66e2d73740a3891bfdeae680c420', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit3d4b66e2d73740a3891bfdeae680c420::getInitializer($loader));
         } else {
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -42,12 +42,17 @@ class ComposerStaticInit3d4b66e2d73740a3891bfdeae680c420
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit3d4b66e2d73740a3891bfdeae680c420::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit3d4b66e2d73740a3891bfdeae680c420::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit3d4b66e2d73740a3891bfdeae680c420::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit3d4b66e2d73740a3891bfdeae680c420::$classMap;
 
         }, null, ClassLoader::class);
     }
